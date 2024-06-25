7d767f1d434faf26ef4a37ac8069f8aa7c01b95a
Horc-zh <horc5555@gmail.com>
2024-06-25 16:29:20 +0800

    the answer of rustlings

---
+++ clippy/target/debug/deps/clippy3-ec091f6ad9dda08a.d
@@ -0,0 +1,9 @@
+/home/horc/code/rustlings/exercises/clippy/target/debug/deps/libclippy3-ec091f6ad9dda08a.rmeta: clippy3.rs Cargo.toml
+
+/home/horc/code/rustlings/exercises/clippy/target/debug/deps/clippy3-ec091f6ad9dda08a.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ variables/variables3.rs
@@ -0,0 +1,8 @@
+// variables3.rs
+//
+// Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
+
+fn main() {
+    let x: i32 = 0;
+    println!("Number {}", x);
+}
